29aba927813279de1d2886ad065fa94478fa7d52
Pratibha Bharti <pratibhabbharti303@gmail.com>
2022-06-25 16:10:58 +0530

    Node js first commit

---
+++ simpleNode.ts
@@ -0,0 +1,10 @@
+import * as http from "http";
+
+const server = http.createServer((request, response) => {
+    if(request.url === "/a"){
+        response.write("api a is invoked");
+    }
+    response.end();
+});
+
+server.listen(process.env.PORT || 3000, () => console.log("simple node created"));
